8f7c1d569bc875529ccd2e277077137c5b9e7591
Zhao Ye <zhaoy@marvell.com>
2015-05-14 10:40:27 +0800

    Revert "clk: mmp: code clean for pxa1936 hwdfc related"
    This reverts commit d97cf548c5e2d85a46adbfc7e0878e08ef937f99.
    
    pxa1936's get_fc_lock's logic is a little different with pxa1908
    we don't need change this function's sequence.
    
    So just remove the redundant hwdfcwr flag.
    
    Change-Id: Ib42811976c6b302a0f971d68f4b959f91f4d2db7
    Signed-off-by: Zhao Ye <zhaoy@marvell.com>

--- drivers/clk/mmp/clk-core-helan3.c
+++ drivers/clk/mmp/clk-core-helan3.c
@@ -361,36 +361,40 @@ static inline void pre_fc(void __iomem *apmu_base)
 	prefc_check_isr(apmu_base);
 }
 
-/* to keep compatible with previous version, just leave *has_hwdfcwr* here. */
 int get_fc_lock(void __iomem *apmu_base)
 {
-	int timeout = 100000;
-	union pmua_dm_cc dm_cc_ap;
+	int timeout;
+	unsigned int reg_val = 0;
 
 	if (atomic_inc_return(&fc_lock_ref_cnt) == 1) {
-		dm_cc_ap.v = __raw_readl(APMU_CCSR(apmu_base));
-		while (timeout) {
-			if (!dm_cc_ap.b.cp_rd_status &&
-					dm_cc_ap.b.ap_rd_status)
-				break;
-			dm_cc_ap.v = __raw_readl(APMU_CCSR(apmu_base));
-			timeout--;
-		}
+		while (1) {
+			timeout = 100000;
 
-		if (unlikely(timeout <= 0)) {
-			pr_err("%s can't get AP lock:CCSR:%x CCR:%x,FCLSR:%x\n",
-			       __func__,
-			       readl(APMU_CCSR(apmu_base)),
-			       readl(APMU_CCR(apmu_base)),
-			       readl(APMU_FCLSR(apmu_base)));
-			WARN_ON(1);
-			return -EAGAIN;
+			do {
+				/* Read dm_cc_ap to compete the FC lock */
+				readl(APMU_CCSR(apmu_base));
+				/* Read FC lock status register to query the lock status */
+				reg_val = readl(APMU_FCLSR(apmu_base));
+				/* AP got FC lock */
+				if ((reg_val & 0x3) == 1)
+					break;
+			} while (timeout--);
+
+			if (unlikely(timeout <= 0)) {
+				pr_err("%s can't get AP lock: CCSR:%x CCR:%x, FCLSR:%x\n",
+				       __func__,
+				       readl(APMU_CCSR(apmu_base)),
+				       readl(APMU_CCR(apmu_base)),
+				       readl(APMU_FCLSR(apmu_base)));
+				WARN_ON(1);
+				return -EAGAIN;
+			}
+
+			return 0;
 		}
-		return 0;
 	}
 
 	pr_err("%s: unmatched fc_lock_ref_cnt\n", __func__);
-
 	return -EINVAL;
 }
 
