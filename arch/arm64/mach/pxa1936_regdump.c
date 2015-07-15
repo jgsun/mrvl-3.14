498a49fe327c21341d027f4b29b0e8d7fb6458b7
huangyh <huangyh@marvell.com>
2015-07-15 13:02:41 +0800

    arm64: mach: dump gic registers when panic
    Dump gic registers when panic for debug.
    
    Change-Id: Ie24b53edd37e68596bae81f393bb6cfd3a16a633
    Signed-off-by: huangyh <huangyh@marvell.com>

--- arch/arm64/mach/pxa1936_regdump.c
+++ arch/arm64/mach/pxa1936_regdump.c
@@ -220,12 +220,113 @@ static void __init mmp_pmum_regdump_init(void)
 	register_regdump_ops(&pmum_regdump_ops);
 }
 
+static struct regdump_ops gic_regdump_ops = {
+	.dev_name = "pxa1936-gic",
+};
+
+static struct regdump_region gic_dump_region[] = {
+	{"GIC_GICD_CTLR",		0x000, 4, regdump_cond_true},
+	{"GIC_GICD_TYPER",		0x004, 4, regdump_cond_true},
+	{"GIC_GICD_IIDR",		0x008, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER0",		0x100, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER1",		0x104, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER2",		0x108, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER3",		0x10c, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER4",		0x110, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER5",		0x114, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER6",		0x118, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER7",		0x11c, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER8",		0x120, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER9",		0x124, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER10",	0x128, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER11",	0x12c, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER12",	0x130, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER13",	0x134, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER14",	0x138, 4, regdump_cond_true},
+	{"GIC_GICD_ISENABLER15",	0x13c, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR0",		0x200, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR1",		0x204, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR2",		0x208, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR3",		0x20c, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR4",		0x210, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR5",		0x214, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR6",		0x218, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR7",		0x21c, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR8",		0x220, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR9",		0x224, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR10",		0x228, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR11",		0x22c, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR12",		0x230, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR13",		0x234, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR14",		0x238, 4, regdump_cond_true},
+	{"GIC_GICD_ISPENDR15",		0x23c, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER0",		0x300, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER1",		0x304, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER2",		0x308, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER3",		0x30c, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER4",		0x310, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER5",		0x314, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER6",		0x318, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER7",		0x31c, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER8",		0x320, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER9",		0x324, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER10",	0x328, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER11",	0x32c, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER12",	0x330, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER13",	0x334, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER14",	0x338, 4, regdump_cond_true},
+	{"GIC_GICD_ISACTIVER15",	0x33c, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR0",		0xc00, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR1",		0xc04, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR2",		0xc08, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR3",		0xc0c, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR4",		0xc10, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR5",		0xc14, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR6",		0xc18, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR7",		0xc1c, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR8",		0xc20, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR9",		0xc24, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR10",		0xc28, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR11",		0xc2c, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR12",		0xc30, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR13",		0xc34, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR14",		0xc38, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR15",		0xc3c, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR16",		0xc40, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR17",		0xc44, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR18",		0xc48, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR19",		0xc4c, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR20",		0xc50, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR21",		0xc54, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR22",		0xc58, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR23",		0xc5c, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR24",		0xc60, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR25",		0xc64, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR26",		0xc68, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR27",		0xc6c, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR28",		0xc70, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR29",		0xc74, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR30",		0xc78, 4, regdump_cond_true},
+	{"GIC_GICD_ICFGR31",		0xc7c, 4, regdump_cond_true},
+};
+
+static void __init mmp_gic_regdump_init(void)
+{
+	gic_regdump_ops.base = gic_get_dist_base();
+	gic_regdump_ops.phy_base = gic_dist_base_phys();
+	gic_regdump_ops.regions = gic_dump_region;
+	gic_regdump_ops.reg_nums = ARRAY_SIZE(gic_dump_region);
+	register_regdump_ops(&gic_regdump_ops);
+}
+
 static int __init mmp_regdump_init(void)
 {
 	if (cpu_is_pxa1936()) {
 		mmp_pmua_regdump_init();
 		mmp_pmum_regdump_init();
+		mmp_gic_regdump_init();
 	}
+
 	return 0;
 }
 arch_initcall(mmp_regdump_init);
